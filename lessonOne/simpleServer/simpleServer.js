f4c0f423f58ea780376bec0f917aacfab1ef3d94
ShakedEfraim <65243259+ShakedEfraim@users.noreply.github.com>
2024-07-15 16:17:54 +0300

    second commit

---
+++ lessonOne/simpleServer/simpleServer.js
@@ -0,0 +1,14 @@
+//Insert http module
+const http = require("http");
+
+//Create a server
+const server = http.createServer((request, response)=>{
+//Create a response
+response.end("Hello world");
+});
+
+//Start the server
+const PORT = 3000;
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
